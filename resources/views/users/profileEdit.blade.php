6f8b0cb3c465c5249ae99039457a53dbed35e105
Aaaaayaka <ayaka.happyplumeria.0819@gmail.com>
2022-11-20 23:53:12 +0900

    no message

---
+++ resources/views/users/profileEdit.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.login')
+
+@section('content')
+
+<div id='container'>
+    <img src="/images/{{ $user->images }}" alt="">
+
+    {!! Form::open(['url' => '/profileEdit/update']) !!}
+
+    <p>UserName</p>
+    {{ Form::label('ユーザー名') }}
+    {{ Form::text('username',$user->username,['class' => 'input']) }}
+
+    <p>MailAddress</p>
+    {{ Form::label('メールアドレス') }}
+    {{ Form::text('mail',$user->mail,['class' => 'input']) }}
+
+    <p>Password</p>
+    {{ Form::label('パスワード') }}
+    {{ Form::text('password',$user->password,['class' => 'input']) }}
+
+    <p>new Password</p>
+    {{ Form::label('新パスワード') }}
+    {{ Form::text('new-password',null,['class' => 'input']) }}
+
+    <p>Bio</p>
+    {{ Form::label('自己紹介') }}
+    {{ Form::text('bio',$user->bio,['class' => 'input']) }}
+
+    <p>Icon Image</p>
+    {{ Form::label('アイコン') }}
+    {{ Form::text('icon',$user->image,['class' => 'input']) }}
+
+    {{ Form::submit('更新', ['class'=>'update_btn']) }}
+
+    {!! Form::close() !!}
+</div>
+
+@endsection
